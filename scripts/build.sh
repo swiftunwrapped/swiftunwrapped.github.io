9e9ac46432ebeeb3cdcbf05b996c96b0d5c83e74
JP Simard <jp@jpsim.com>
2020-11-14 23:18:53 -0500

    Revert changes to readme, add episodes to index.md instead

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,10 +9,8 @@ readonly episodes="$(curl \
   --request GET \
   'https://api.simplecast.com/podcasts/c0a958c6-59dd-45ad-b246-1b7a1a9e51f6/episodes?limit=200')"
 
-cat > README.md <<- EOF
-# Swift Unwrapped
-
-A podcast spin off of Swift Weekly Brief and other Swifty news.
+cat > index.md <<- EOF
+*{{ site.description }}*
 
 EOF
 
@@ -22,5 +20,5 @@ for episode in $(echo "${episodes}" | jq -r '.collection[] | @base64'); do
     echo ${episode} | base64 --decode | jq -r ${1}
   }
 
-  echo "* [$(_jq '.title')](https://spec.fm/podcasts/swift-unwrapped/$(_jq '.token'))" >> README.md
+  echo "* [$(_jq '.title')](https://spec.fm/podcasts/swift-unwrapped/$(_jq '.token'))" >> index.md
 done
